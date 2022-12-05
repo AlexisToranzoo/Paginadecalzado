fc75db1cefbe8ab902ff92bc2d7435e916a8dc0d
Alexis Toranzo <102900171+AlexisToranzoo@users.noreply.github.com>
2022-12-05 17:29:04 -0300

    Update newconnect.js

--- conexion/newconnect.js
+++ conexion/newconnect.js
@@ -2,7 +2,11 @@ var mysql= require("mysql")
 
 var con=mysql.createConnection(
 {
-   
+    host:'sql10.freesqldatabase.com',
+    user:'sql10583013',
+    password:'KRfwIUHFBT',
+    port:'3306',
+    database:'sql10583013'
 }
 )
 
